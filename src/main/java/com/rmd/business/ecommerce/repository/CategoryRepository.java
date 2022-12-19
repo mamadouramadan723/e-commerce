fa8e941e4e7c91c2af536171f1fa45f60a37790e
diallo <72153331+mamadouramadan723@users.noreply.github.com>
2022-12-19 22:56:41 +0100

    1st Commit

---
+++ src/main/java/com/rmd/business/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.rmd.business.ecommerce.repository;
+
+import com.rmd.business.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Category findByCategoryName(String categoryName);
+
+}
